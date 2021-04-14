ccd38da48fe53d9ad2c1a1ceedbaa02fbe7107e5
Maksim Zinal <mzinal@ru.ibm.com>
2021-04-14 15:08:34 +0300

    переименовал каталог по проекту

---
+++ dcs-ru/scripts/dcs-remove.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+call dcs-config.cmd
+
+@echo on
+
+cmd /c %IISROOT%\%IAADMIN% -user %IAUSER% -password %IAPASS% -url %IISURL% -undeployDataClasses %JARNAME%
--- russian-dcs/scripts/dcs-remove.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call dcs-config.cmd
-
-@echo on
-
-cmd /c %IISROOT%\%IAADMIN% -user %IAUSER% -password %IAPASS% -url %IISURL% -undeployDataClasses %JARNAME%
