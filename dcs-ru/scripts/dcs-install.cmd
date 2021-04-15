937aae62abaac12132ce9a0a1385b898ed406754
Maksim Zinal <mzinal@ru.ibm.com>
2021-04-15 10:28:02 +0300

    управление концами строк для файлов + мелкие правки скриптов

--- dcs-ru/scripts/dcs-install.cmd
+++ dcs-ru/scripts/dcs-install.cmd
@@ -10,3 +10,5 @@ copy /Y %JARSRC% %IISROOT%\%IISJAR2%\%JARDST%
 cmd /c %IISROOT%\%IAADMIN% -user %IAUSER% -password %IAPASS% -url %IISURL% -updateDataClasses %JARNAME%
 
 cmd /c %IISROOT%\%IAADMIN% -installClassifiers %JARNAME%
+
+pause
--- dcs-ru/scripts/dcs-remove.cmd
+++ dcs-ru/scripts/dcs-remove.cmd
@@ -5,3 +5,5 @@ call dcs-config.cmd
 @echo on
 
 cmd /c %IISROOT%\%IAADMIN% -user %IAUSER% -password %IAPASS% -url %IISURL% -undeployDataClasses %JARNAME%
+
+pause
